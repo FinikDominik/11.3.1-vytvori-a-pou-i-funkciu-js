e4ee5bf6e403c39bbc1bbe7f740947d8064b4ea3
Dominik Fínik <92373597+FinikDominik@users.noreply.github.com>
2022-05-05 16:25:42 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,21 @@
+alert("Javaskript loaded!")
+
+let a = 4;
+let b = 2;
+
+console.log( "Hodnota a = " + a, "Hodnota b = " + b );
+console.log( 'Vykonávam matematickie operácie' );
+console.log( 'a + b = ' + (a + b )) ;
+console.log( 'a - b = ' + (a - b )) ;
+console.log( 'a * b = ' + a * b ) ;
+console.log( 'a / b = ' + a / b ) ;
+
+let c = 9;
+let d = 8;
+
+console.log( "Hodnota a = " + a, "Hodnota b = " + b );
+console.log( 'Vykonávam matematickie operácie' );
+console.log( 'a + b = ' + (a + b )) ;
+console.log( 'a - b = ' + (a - b )) ;
+console.log( 'a * b = ' + a * b ) ;
+console.log( 'a / b = ' + a / b ) ;
